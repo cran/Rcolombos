379d977d05cdac6c3749cf65ebfa8015ac1bfe30
Paolo Sonego <paolo.sonego@fmach.it>
2013-06-26 00:00:00 +0000

    version 1.1

--- R/utilities.R
+++ R/utilities.R
@@ -128,3 +128,56 @@ listContrasts <- function(organism="ecoli"){
     return(response)
   }
 }
+
+#' This method allows to download/import the full compendium for the selected organism
+#'
+#' @param organism A string containing the organism of interest.
+#'
+#' @param path A string indicating the path where the file will be either downloaded or read,
+#' if already retrieved
+#' 
+#'
+#' @return A data.frame containing the full compendium for the selected organism.
+#'
+#' @references http://colombos.net
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library('Rcolombos')
+#' getCompendium()
+#' }
+#'
+getCompendium <- function(organism="ecoli", path=NULL){
+  if(is.null(path)) path <- getwd() else {}
+    destfile <- paste(path,"/",organism, "_compendium_data.txt.gz",sep="")
+    header.field = c('Content-Type' = "application/json")
+    curl <- getCurlHandle() 
+    curlSetOpt(.opts = list(httpheader = header.field, verbose = FALSE), curl = curl) 
+    t <- basicTextGatherer()
+    h <- basicHeaderGatherer()
+    body = curlPerform(url=paste("http://rest.colombos.net/get_organism_data/", organism, "/",sep=""),
+                     curl = curl,
+                     writefunction = t$update,
+                     headerfunction = h$update)
+    output <- list(data = t$value(),
+                 status = h$value()[['status']],
+                 status.message = h$value()[['statusMessage']])
+    httpstatus <- as.numeric(output$status)
+    if (httpstatus != 200) {
+      return(output$status.message)
+    } else {
+      if(!file.exists(destfile)){
+        tmp <- fromJSON(output$data, nullValue = NA)$data;
+        download.file( tmp, destfile )
+#       return(tmp)
+      } else {}
+    ## TO.DO create a ExpressionSet object take care of all contrasts and genes info
+    my_cols <- scan(destfile, what="c", nlines=1)
+    out <- read.csv(destfile, row.names=1,
+                    skip=7, stringsAsFactors=FALSE, sep="\t")
+    out <- out[,c(2:dim(out)[[2]])] 
+    colnames(out) = my_cols; out <- out[,c(2:dim(out)[[2]])]
+  }    
+}
